47f0f46a3066f54124799f8bbae14f3f866ce963
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2023-02-22 09:42:13 +0700

    init

---
+++ src/main/kotlin/com/dicoding/kotlin/App.kt
@@ -0,0 +1,11 @@
+/*
+data class User(val name: String, val age: Int)
+
+- the main constructor on that class at least have one params
+- all main constructor must declared as val or var
+- modifier on a data class can't be abstract, open, sealed, or inner
+*/
+
+fun main() {
+
+}
